505b18d1800bbaee908a66790a13c3a26dc99988
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-12 15:57:59 +0200

    Added 2 new components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <script>
+import RangeInput from './components/RangeInput.vue';
+import Image from './components/Image.vue';
+
 export default {
-    
+    components: {
+        RangeInput,
+        Image
+    }
 }
 </script>
 
 <template>
     <h1>Update CSS Variables with <span class="yellow-txt">JS</span></h1>
+    <RangeInput />
+    <Image />
 </template>
 
 <style scoped>
     h1 {
         font-size: 3rem;
         text-align: center;
+        margin: auto;
         color: white;
         grid-column: 1/4;
+        user-select: none;
     }
 
     .yellow-txt {
