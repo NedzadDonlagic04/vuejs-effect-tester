a2e672af89aa8dd3a4c5498d13581f45114981ff
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-12 15:58:31 +0200

    Added 2 new components
    One to represent the image and one for the range
    input.

---
+++ src/components/Image.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div class="img-container">
+        <img src="./../assets/img/hike.jpeg" alt="person hiking">
+    </div>
+</template>
+
+<style scoped>
+    img {
+        max-width: 100%;
+        max-height: 100%;
+    }
+
+    .img-container {
+        background-color: black;
+        height: 31rem;
+        grid-row: 3/4;
+        grid-column: 1/4;
+        margin: auto;
+    }
+</style>
---
+++ src/components/RangeInput.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div class="var-container">
+        
+    </div>
+</template>
+
+<style scoped>
+    .var-container {
+        height: 100%;
+        width: 100%;
+
+        display: flex;
+        flex-direction: row;
+    }
+</style>
