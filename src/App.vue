b602a1bba86875aa8e9367cc302165b15ae14bfc
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-12 14:26:42 +0200

    Styled H1 element

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,18 @@ export default {
 </script>
 
 <template>
-    <h1>Hello world!</h1>
+    <h1>Update CSS Variables with <span class="yellow-txt">JS</span></h1>
 </template>
 
 <style scoped>
     h1 {
-        font-size: 2rem;
+        font-size: 3rem;
+        text-align: center;
         color: white;
+        grid-column: 1/4;
+    }
+
+    .yellow-txt {
+        color: hsl(44, 100%, 50%);
     }
 </style>
