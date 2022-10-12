c7bf2cb0881a806620684047335de166929820c6
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-13 00:07:50 +0200

    Added component that represents color input

---
+++ src/components/ColorInput.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            required: true
+        }
+    },
+    emits: ['change'],
+    methods: {
+        updateColor(event)
+        {
+            this.$emit('change', event.target.value);
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="var-container">
+        <label for="color">{{text}}</label>
+        <input @input="updateColor" type="color">
+    </div>
+</template>
+
+<style scoped>
+    .var-container {
+        height: 100%;
+        width: 100%;
+        color: white;
+        user-select: none;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+
+        accent-color: var(--yellow-base);
+    }
+
+    .var-container label {
+        font-size: 1.5rem;
+    }
+
+    .var-container label::first-letter {
+        text-transform: uppercase;
+    }
+
+    input[type="color"]:hover {
+        cursor: pointer;
+    }
+</style>
