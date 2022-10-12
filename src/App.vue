6230236e30955ec299051a3728d388e09a7706c9
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-12 13:00:37 +0200

    Added basic tags as well as Hello World message

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+    <h1>Hello world!</h1>
+</template>
+
+<style scoped>
+    h1 {
+        font-size: 2rem;
+        color: white;
+    }
+</style>
