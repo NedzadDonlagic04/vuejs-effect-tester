1fbcb931d558c0064d5391b848081086c2907693
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-13 00:08:24 +0200

    Added option to change background color

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -8,13 +8,17 @@ export default {
         space: {
             type: String,
             required: true
+        },
+        color: {
+            type: String,
+            required: true
         }
     }
 }
 </script>
 
 <template>
-    <div class="img-container" :style="{ padding: space + 'rem', filter: `blur(${blur}px)`}">
+    <div class="img-container" :style="{ backgroundColor: color, padding: 1/2*space + 'rem ' + space + 'rem', filter: `blur(${blur}px)`}">
         <img src="./../assets/img/hike.jpeg" alt="person hiking">
     </div>
 </template>
@@ -26,8 +30,8 @@ export default {
     }
 
     .img-container {
-        user-select: none;
         background-color: black;
+        user-select: none;
         height: 31rem;
         grid-row: 3/4;
         grid-column: 1/4;
